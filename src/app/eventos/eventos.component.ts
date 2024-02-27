12bbfc8da8777286d5fdb813052824861581e694
isaque524 <isaquesilvadamasceno@gmail.com>
2024-02-27 16:53:24 -0300

    eventos tabela

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -8,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  LarguraImagem: number = 150;
+  margemImagem: number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -16,11 +41,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarValorImagem() {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos(): void {
     this.http
       .get('https://isqeventosapi.azurewebsites.net/api/evento')
       .subscribe(
-        (response) => (this.eventos = response),
+        (response) => {
+          (this.eventos = response), (this.eventosFiltrados = this.eventos);
+        },
         (error) => console.log(error)
       );
   }
